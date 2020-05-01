20aba5b74ad4b17fe6ec922c92e2c853bf87af08
arlan-trislis <arlanariandi@gmail.com>
2020-05-01 15:57:36 +0700

    add row and column project

---
+++ section-6/rowandcolumn/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: MyApp(),
+    ));
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+              width: 100.0,
+              color: Colors.red,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.yellow,
+                ),
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.green,
+                )
+              ],
+            ),
+            Container(
+              width: 100.0,
+              color: Colors.blue,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
